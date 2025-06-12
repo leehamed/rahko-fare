333605482e5f8d579a6ce1058b0229e8415dd611
macbook <politecoder97@gmail.com>
2025-06-12 09:30:38 +0330

    save login in web cookies

--- lib/main.dart
+++ lib/main.dart
@@ -1,21 +1,19 @@
 import 'package:flutter/material.dart';
-import 'package:webview_flutter/webview_flutter.dart';
+import 'package:flutter_inappwebview/flutter_inappwebview.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext c) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-      ),
       home: const MyHomePage(),
     );
   }
@@ -25,120 +23,69 @@ class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key});
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  _MyHomePageState createState() => _MyHomePageState();
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  late final WebViewController _controller;
+  late InAppWebViewController webViewController;
+
+  @override
+  Widget build(BuildContext c) {
+    return Scaffold(
+      body: SafeArea(
+        child: InAppWebView(
+          initialUrlRequest: URLRequest(url: WebUri('https://fare.rahko.ir')),
+          initialSettings: InAppWebViewSettings(
+            javaScriptEnabled: true,
+            cacheEnabled: true,
+            sharedCookiesEnabled: true,
+            thirdPartyCookiesEnabled: true,
+          ),
+          initialOptions: InAppWebViewGroupOptions(
+            android: AndroidInAppWebViewOptions(useHybridComposition: true),
+            ios: IOSInAppWebViewOptions(sharedCookiesEnabled: true),
+          ),
+          onWebViewCreated: (ctrl) {
+            webViewController = ctrl;
+          },
+          onLoadStop: (ctrl, url) async {
+            await saveCookies();
+          },
+        ),
+      ),
+    );
+  }
+
+  Future<void> saveCookies() async {
+    final uri = WebUri('https://fare.rahko.ir');
+    List<Cookie> cookies = await CookieManager.instance().getCookies(url: uri);
+    final prefs = await SharedPreferences.getInstance();
+    final cookieString = cookies.map((c) => '${c.name}=${c.value}').join('; ');
+    await prefs.setString('saved_cookies', cookieString);
+  }
+
+  Future<void> restoreCookies() async {
+    final prefs = await SharedPreferences.getInstance();
+    final cookieString = prefs.getString('saved_cookies');
+    if (cookieString != null) {
+      final uri = WebUri('https://fare.rahko.ir');
+      for (var pair in cookieString.split('; ')) {
+        final parts = pair.split('=');
+        if (parts.length == 2) {
+          await CookieManager.instance().setCookie(
+            url: uri,
+            name: parts[0],
+            value: parts[1],
+            domain: 'fare.rahko.ir',
+          );
+        }
+      }
+    }
+  }
 
   @override
   void initState() {
     super.initState();
-
-    late final PlatformWebViewControllerCreationParams params;
-    params = const PlatformWebViewControllerCreationParams();
-
-    final WebViewController controller =
-        WebViewController.fromPlatformCreationParams(params);
-
-    controller
-      ..setJavaScriptMode(JavaScriptMode.unrestricted)
-      ..setNavigationDelegate(
-        NavigationDelegate(
-          onProgress: (int progress) {
-            debugPrint('WebView is loading (progress : $progress%)');
-          },
-          onPageStarted: (String url) {
-            debugPrint('Page started loading: $url');
-          },
-          onPageFinished: (String url) {
-            debugPrint('Page finished loading: $url');
-          },
-          onWebResourceError: (WebResourceError error) {
-            debugPrint('''
-Page resource error:
-  code: ${error.errorCode}
-  description: ${error.description}
-  errorType: ${error.errorType}
-  isForMainFrame: ${error.isForMainFrame}
-          ''');
-          },
-          onHttpError: (HttpResponseError error) {
-            debugPrint('Error occurred on page: ${error.response?.statusCode}');
-          },
-          onUrlChange: (UrlChange change) {
-            debugPrint('url change to ${change.url}');
-          },
-          onHttpAuthRequest: (HttpAuthRequest request) {
-            openDialog(request);
-          },
-        ),
-      )
-      ..loadRequest(Uri.parse('https://fare.rahko.ir'));
-
-    _controller = controller;
-  }
-
-
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: SafeArea(child: WebViewWidget(controller: _controller)),
-    );
-  }
-
-  Future<void> openDialog(HttpAuthRequest httpRequest) async {
-    final TextEditingController usernameTextController =
-    TextEditingController();
-    final TextEditingController passwordTextController =
-    TextEditingController();
-
-    return showDialog(
-      context: context,
-      barrierDismissible: false,
-      builder: (BuildContext context) {
-        return AlertDialog(
-          title: Text('${httpRequest.host}: ${httpRequest.realm ?? '-'}'),
-          content: SingleChildScrollView(
-            child: Column(
-              mainAxisSize: MainAxisSize.min,
-              children: <Widget>[
-                TextField(
-                  decoration: const InputDecoration(labelText: 'Username'),
-                  autofocus: true,
-                  controller: usernameTextController,
-                ),
-                TextField(
-                  decoration: const InputDecoration(labelText: 'Password'),
-                  controller: passwordTextController,
-                ),
-              ],
-            ),
-          ),
-          actions: <Widget>[
-            TextButton(
-              onPressed: () {
-                httpRequest.onCancel();
-                Navigator.of(context).pop();
-              },
-              child: const Text('Cancel'),
-            ),
-            TextButton(
-              onPressed: () {
-                httpRequest.onProceed(
-                  WebViewCredential(
-                    user: usernameTextController.text,
-                    password: passwordTextController.text,
-                  ),
-                );
-                Navigator.of(context).pop();
-              },
-              child: const Text('Authenticate'),
-            ),
-          ],
-        );
-      },
-    );
+    restoreCookies().then((_) => setState(() {}));
   }
 }
